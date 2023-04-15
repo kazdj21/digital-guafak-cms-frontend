758ef975672d94a26e4c803e9efb151a60669060
Kaz <kazdj21@hotmail.com>
2023-04-15 22:04:24 +1000

    Fixed spacing for each item listing

--- src/components/CardSet.js
+++ src/components/CardSet.js
@@ -3,7 +3,7 @@ import { Row, Col, Container, CardGroup } from "react-bootstrap"
 function CardSet({ children, paddingY, paddingX, marginX, marginY, useRows, cardSetStyle }) {
 
     return <CardGroup className={`px-${paddingX || 0} py-${paddingY || 0} mx-${marginX || 0} my-${marginY || 0}`} style={{minHeight: "10vh", margin: 0, padding: 0, ...cardSetStyle}}>
-        { useRows? [children].map((item) => <Row style={{padding: 0, margin: 0}}>{item}</Row>) : [children].map((item) => item)}
+        { useRows? [children].map((item) => <Row style={{padding: 0, margin: 0, width: "90vw"}}>{item}</Row>) : [children].map((item) => item)}
     </CardGroup>
 
 }
